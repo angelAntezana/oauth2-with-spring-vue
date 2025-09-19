1f26a82ebe9abf0d61bf66359681b56c37b37d46
angelAntezana <angel2estevan@gmail.com>
2025-09-19 12:38:26 +0000

    added project spring

---
+++ backend-oauth2/src/test/java/com/demo/oauth/aeas/DemoOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.oauth.aeas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
